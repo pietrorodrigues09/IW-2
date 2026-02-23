1b59115fff70b833138e6f7d6a3df2586a614b3f
pietrorodrigues09 <pietrorodriguesetec2025@gmail.com>
2026-02-23 08:46:00 -0300

    aula dia 23/02

---
+++ aulajs/js/index.js
@@ -0,0 +1,1 @@
+console.log("Gemedor da sala");
---
+++ aulajs/js/variaveis.js
@@ -0,0 +1,20 @@
+var nomedavariavel = "valor da variavel";
+var nomedaOUTRAvariavel = 'valor da  outra variavel';
+var variavelIdade = 22 ;
+var variavelTenhoCabeloBranco = false;
+var variavelIndefinida ;
+
+console.log (nomedavariavel);
+console.log (nomedaOUTRAvariavel);
+console.log (variavelIdade);
+console.log (variavelTenhoCabeloBranco);
+console.log (variavelIndefinida);
+
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+console.log(fl);
