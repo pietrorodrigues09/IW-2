1c6248649867d09308d5f914b8de6d4096af7499
pietrorodrigues09 <pietrorodriguesetec2025@gmail.com>
2026-03-02 08:46:29 -0300

    Add files via upload

---
+++ aula2.2/js/index.js
@@ -0,0 +1,9 @@
+let resposta = confirm("Você escolheu continuar?");
+if (resposta) {
+    alert("Você escolheu continuar.");
+} else{
+    alert("Você cancelou a ação.")
+}
+
+let nome = prompt("Qual é o seu nome?");
+alert("Olá," +nome+" !");
