e7ecd1c28e60034b4a8cd85fdec0cffb935353c8
pietrorodrigues09 <pietrorodriguesetec2025@gmail.com>
2026-03-02 08:06:04 -0300

    Add files via upload

---
+++ aula02/javascript/index.js
@@ -0,0 +1,40 @@
+console.log("Exercicio A")
+var a = 10;
+var b = 20;
+console.log(b);
+var b = 20 - 15;
+console.log(a , b);
+
+console.log("Exercicio B")
+var a = 30;
+var b = 20;
+var c = a + b;
+console.log(c);
+b = 10;
+console.log(b,c);
+var c = a + b;
+console.log(a,b,c);
+
+console.log("Exercicio C")
+var a = 10;
+var b = 20;
+var c = a;
+var b = c;
+var a = b;
+console.log(a, b, c)
+
+console.log("Exercicio D")
+var a = 10;
+var b = a+1;
+var a = b+1;
+var b = a+1;
+console.log(a);
+a = b+1;
+console.log(a,b);
+
+console.log("Exercicio 2")
+var n1 = 2;
+var n2 = 3;
+var n3 = 5;
+var media = (n1*2 + n2*3 + n3*5)/10;
+console.log(media)
